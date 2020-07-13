1a2046590bb3fa2576a8e166e0b0c2f2b5dd2111
Herbert Poul <herbert@codeux.design>
2020-07-13 13:13:20 +0200

    create windows plugin from flutter template.

---
+++ example/windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,12 @@
+//
+//  Generated file. Do not edit.
+//
+
+#include "generated_plugin_registrant.h"
+
+#include <argon2_ffi/argon2_ffi_plugin.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  Argon2FfiPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("Argon2FfiPlugin"));
+}
