e77fefca9fa56992673808455809a2de88caab0e
Herbert Poul <herbert@codeux.design>
2023-08-19 22:08:43 +0200

    run with flutter 3.13

--- example/linux/flutter/generated_plugin_registrant.cc
+++ example/linux/flutter/generated_plugin_registrant.cc
@@ -2,11 +2,14 @@
 //  Generated file. Do not edit.
 //
 
+// clang-format off
+
 #include "generated_plugin_registrant.h"
 
-#include <argon2_ffi_plugin.h>
+#include <argon2_ffi/argon2_ffi_plugin.h>
 
-void RegisterPlugins(flutter::PluginRegistry* registry) {
-  Argon2FfiPluginRegisterWithRegistrar(
-      registry->GetRegistrarForPlugin("Argon2FfiPlugin"));
+void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) argon2_ffi_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "Argon2FfiPlugin");
+  argon2_ffi_plugin_register_with_registrar(argon2_ffi_registrar);
 }
