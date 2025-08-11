c4215e5b30a9669314bc6385ede7d8d69c00d8fd
Herbert Poul <herbert@codeux.design>
2025-08-11 12:29:32 +0200

    Upgrade to flutter 3.32.8 (gradle plugin, deprecated classes, etc)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.0+2
+
+* Upgrade to flutter 3.32.8 (gradle plugin, deprecated classes, etc)
+
 ## 1.0.0+1
 
 * Upgrade to flutter 3.13, upgrade android gradle plugin, etc.
--- android/src/main/kotlin/com/example/argon2_ffi/Argon2FfiPlugin.kt
+++ android/src/main/kotlin/com/example/argon2_ffi/Argon2FfiPlugin.kt
@@ -6,7 +6,6 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** Argon2FfiPlugin */
 class Argon2FfiPlugin: FlutterPlugin, MethodCallHandler {
